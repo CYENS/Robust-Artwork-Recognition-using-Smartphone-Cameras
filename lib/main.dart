87a6b1f5a971e0d1af5e8523051d0afabf015c75
marios <msthoma@yahoo.com>
2020-08-08 18:07:46 +0300

    move launching app to separate class

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,26 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:modern_art_app/home.dart';
 
+CameraDescription camera;
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final firstCamera = (await availableCameras()).first;
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData.dark(),
-    home: HomePage(camera: firstCamera),
-  ));
+  try {
+    // get back camera
+    camera = (await availableCameras()).first;
+  } on CameraException catch (e) {
+    print("Error ${e.code}\nError msg: ${e.description}");
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: HomePage(camera: camera),
+    );
+  }
 }
