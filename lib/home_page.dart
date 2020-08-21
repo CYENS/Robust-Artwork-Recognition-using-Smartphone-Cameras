5091d05fa5b641ff9b6ec3e9ea7e71d2e2af1eb7
marios <msthoma@yahoo.com>
2020-08-21 12:13:36 +0300

    wrap body with SafeArea

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -27,59 +27,61 @@ class HomePage extends StatelessWidget {
 //          )
 //        ],
 //      ),
-      body: Container(
-        color: Colors.black,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: size.height * 0.5,
-              width: size.width,
-              child: Stack(
-                children: [
-                  Container(
-                    height: size.height * 0.5,
-                    width: size.width,
-                    child: Image.asset(
-                      "pinakothiki_building.jpg",
-                      fit: BoxFit.fitHeight,
-                    ),
-                  ),
-                  Container(
-                    // add gradient in front of building photo to make text in
-                    // front of it legible, based on the example provided here
-                    // https://api.flutter.dev/flutter/widgets/Stack-class.html
-                    alignment: Alignment.bottomRight,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: <Color>[
-                          Colors.black.withAlpha(0),
-                          Colors.black12,
-                          Colors.black38,
-                          Colors.black
-                        ],
+      body: SafeArea(
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: size.height * 0.5,
+                width: size.width,
+                child: Stack(
+                  children: [
+                    Container(
+                      height: size.height * 0.5,
+                      width: size.width,
+                      child: Image.asset(
+                        "pinakothiki_building.jpg",
+                        fit: BoxFit.fitHeight,
                       ),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 20, horizontal: 8),
-                      child: Text(
-                        "Κρατική Πινακοθήκη\nΣύγχρονης Κυπριακής Τέχνης",
-                        style: TextStyle(fontSize: 30),
-                        textAlign: TextAlign.end,
+                    Container(
+                      // add gradient in front of building photo to make text in
+                      // front of it legible, based on the example provided here
+                      // https://api.flutter.dev/flutter/widgets/Stack-class.html
+                      alignment: Alignment.bottomRight,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: <Color>[
+                            Colors.black.withAlpha(0),
+                            Colors.black12,
+                            Colors.black38,
+                            Colors.black
+                          ],
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 8),
+                        child: Text(
+                          "Κρατική Πινακοθήκη\nΣύγχρονης Κυπριακής Τέχνης",
+                          style: TextStyle(fontSize: 30),
+                          textAlign: TextAlign.end,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            HeadlineAndMoreRow(listType: "Painting"),
-            PaintingListHorizontal(listType: "Painting"),
-            HeadlineAndMoreRow(listType: "Painter"),
-            PaintingListHorizontal(listType: "Painter"),
-          ],
+              HeadlineAndMoreRow(listType: "Painting"),
+              PaintingListHorizontal(listType: "Painting"),
+              HeadlineAndMoreRow(listType: "Painter"),
+              PaintingListHorizontal(listType: "Painter"),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
