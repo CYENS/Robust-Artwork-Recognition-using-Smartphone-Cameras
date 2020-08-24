1a48c3c86e104e672e82bccc1962a59ea389077d
marios <msthoma@yahoo.com>
2020-08-24 10:29:51 +0300

    remove FAB, add bottom navigation bar to home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,14 +3,30 @@ import 'package:flutter/material.dart';
 import 'package:modern_art_app/camera.dart';
 import 'package:modern_art_app/painting_list.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final List<CameraDescription> cameras;
 
   const HomePage({Key key, @required this.cameras}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    List<Widget> _screens = [
+      IntroPage(),
+      TakePictureScreen(cameras: widget.cameras)
+    ];
     return Scaffold(
 //      appBar: AppBar(
 //        title: Text("Modern Art App"),
@@ -27,71 +43,77 @@ class HomePage extends StatelessWidget {
 //          )
 //        ],
 //      ),
-      body: SafeArea(
-        child: Container(
-          color: Colors.black,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: size.height * 0.5,
-                width: size.width,
-                child: Stack(
-                  children: [
-                    Positioned.fill(
-                      child: Image.asset(
-                        "pinakothiki_building.jpg",
-                        fit: BoxFit.fitHeight,
-                      ),
-                    ),
-                    Container(
-                      // add gradient in front of building photo to make text in
-                      // front of it legible, based on the example provided here
-                      // https://api.flutter.dev/flutter/widgets/Stack-class.html
-                      alignment: Alignment.bottomRight,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: <Color>[
-                            Colors.black.withAlpha(0),
-                            Colors.black12,
-                            Colors.black38,
-                            Colors.black
-                          ],
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 20, horizontal: 8),
-                        child: Text(
-                          "Κρατική Πινακοθήκη\nΣύγχρονης Κυπριακής Τέχνης",
-                          style: TextStyle(fontSize: 30),
-                          textAlign: TextAlign.end,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              HeadlineAndMoreRow(listType: "Painting"),
-              PaintingListHorizontal(listType: "Painting"),
-              HeadlineAndMoreRow(listType: "Painter"),
-              PaintingListHorizontal(listType: "Painter"),
-            ],
-          ),
-        ),
+      body: _screens.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home), title: Text("Explore")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.camera), title: Text("Identify")),
+        ],
+        onTap: _onItemTapped,
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        label: Text("Αναγνώριση Πίνακα"),
-        icon: Icon(Icons.camera_alt),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => TakePictureScreen(cameras: cameras)),
-          );
-        },
+    );
+  }
+}
+
+class IntroPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Container(
+        color: Colors.black,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: size.height * 0.45,
+              width: size.width,
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: Image.asset(
+                      "pinakothiki_building.jpg",
+                      fit: BoxFit.fitHeight,
+                    ),
+                  ),
+                  Container(
+                    // add gradient in front of building photo to make text in
+                    // front of it legible, based on the example provided here
+                    // https://api.flutter.dev/flutter/widgets/Stack-class.html
+                    alignment: Alignment.bottomRight,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: <Color>[
+                          Colors.black.withAlpha(0),
+                          Colors.black12,
+                          Colors.black38,
+                          Colors.black
+                        ],
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20, horizontal: 8),
+                      child: Text(
+                        "Κρατική Πινακοθήκη\nΣύγχρονης Κυπριακής Τέχνης",
+                        style: TextStyle(fontSize: 30),
+                        textAlign: TextAlign.end,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            HeadlineAndMoreRow(listType: "Painting"),
+            PaintingListHorizontal(listType: "Painting"),
+            HeadlineAndMoreRow(listType: "Painter"),
+            PaintingListHorizontal(listType: "Painter"),
+          ],
+        ),
       ),
     );
   }
@@ -107,7 +129,7 @@ class HeadlineAndMoreRow extends StatelessWidget {
   Widget build(BuildContext context) => Padding(
         padding: const EdgeInsets.fromLTRB(8, 24, 8, 8),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
+//          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             headline("${listType}s"),
             Spacer(),
