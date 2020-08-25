7f88fc4c2b2e8028aa5386b588d944f1e87d0c3a
marios <msthoma@yahoo.com>
2020-08-25 15:12:29 +0300

    example file from moor library with data classes and database declaration

---
+++ lib/db/database.dart
@@ -0,0 +1,29 @@
+import 'package:moor/moor.dart';
+
+part 'database.g.dart';
+
+// this will generate a table called "todos" for us. The rows of that table will
+// be represented by a class called "Todo".
+class Todos extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get title => text().withLength(min: 6, max: 32)();
+  TextColumn get content => text().named('body')();
+  IntColumn get category => integer().nullable()();
+}
+
+// This will make moor generate a class called "Category" to represent a row in this table.
+// By default, "Categorie" would have been used because it only strips away the trailing "s"
+// in the table name.
+@DataClassName("Category")
+class Categories extends Table {
+
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get description => text()();
+}
+
+// this annotation tells moor to prepare a database class that uses both of the
+// tables we just defined. We'll see how to use that database class in a moment.
+@UseMoor(tables: [Todos, Categories])
+class MyDatabase {
+
+}
