137f4bf7f20c5a038d99de18161555a192cd76cf
marios <msthoma@yahoo.com>
2020-08-25 15:43:51 +0300

    modify Database class and add example functions for getting all items from db

--- lib/db/database.dart
+++ lib/db/database.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:moor/ffi.dart';
 import 'package:moor/moor.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
 
 part 'database.g.dart';
 
@@ -11,8 +16,11 @@ part 'database.g.dart';
 // be represented by a class called "Todo".
 class Todos extends Table {
   IntColumn get id => integer().autoIncrement()();
+
   TextColumn get title => text().withLength(min: 6, max: 32)();
+
   TextColumn get content => text().named('body')();
+
   IntColumn get category => integer().nullable()();
 }
 
@@ -21,14 +29,38 @@ class Todos extends Table {
 // in the table name.
 @DataClassName("Category")
 class Categories extends Table {
-
   IntColumn get id => integer().autoIncrement()();
+
   TextColumn get description => text()();
 }
 
-// this annotation tells moor to prepare a database class that uses both of the
-// tables we just defined. We'll see how to use that database class in a moment.
-@UseMoor(tables: [Todos, Categories])
-class MyDatabase {
+LazyDatabase _openConnection() {
+  // the LazyDatabase util lets us find the right location for the file async.
+  return LazyDatabase(() async {
+    // put the database file, called db.sqlite here, into the documents folder
+    // for your app.
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return VmDatabase(file);
+  });
+}
 
+@UseMoor(tables: [Todos, Categories])
+class MyDatabase extends _$MyDatabase {
+  // we tell the database where to store the data with this constructor
+  MyDatabase() : super(_openConnection());
+
+  // you should bump this number whenever you change or add a table definition. Migrations
+  // are covered later in this readme.
+  @override
+  int get schemaVersion => 1;
+
+  // loads all todo entries
+  Future<List<Todo>> get allTodoEntries => select(todos).get();
+
+  // watches all todo entries in a given category. The stream will automatically
+  // emit new items whenever the underlying data changes.
+  Stream<List<Todo>> watchEntriesInCategory(Category c) {
+    return (select(todos)..where((t) => t.category.equals(c.id))).watch();
+  }
 }
