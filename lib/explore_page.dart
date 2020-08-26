f09ec121cefd82854272e2d3c4fc03c27f396ac0
marios <msthoma@yahoo.com>
2020-08-26 11:32:57 +0300

    make widget scrollable

--- lib/explore_page.dart
+++ lib/explore_page.dart
@@ -6,57 +6,60 @@ class ExplorePage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SafeArea(
-      child: Container(
-        color: Colors.black,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: size.height * 0.45,
-              width: size.width,
-              child: Stack(
-                children: [
-                  Positioned.fill(
-                    child: Image.asset(
-                      "assets/pinakothiki_building.jpg",
-                      fit: BoxFit.fitHeight,
-                    ),
-                  ),
-                  Container(
-                    // add gradient in front of building photo to make text in
-                    // front of it legible, based on the example provided here
-                    // https://api.flutter.dev/flutter/widgets/Stack-class.html
-                    alignment: Alignment.bottomRight,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: <Color>[
-                          Colors.black.withAlpha(0),
-                          Colors.black12,
-                          Colors.black38,
-                          Colors.black
-                        ],
+      child: SingleChildScrollView(
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: size.height * 0.45,
+                width: size.width,
+                child: Stack(
+                  children: [
+                    Positioned.fill(
+                      child: Image.asset(
+                        "assets/pinakothiki_building.jpg",
+                        fit: BoxFit.fitHeight,
                       ),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 20, horizontal: 8),
-                      child: Text(
-                        "Κρατική Πινακοθήκη\nΣύγχρονης Κυπριακής Τέχνης",
-                        style: TextStyle(fontSize: 30),
-                        textAlign: TextAlign.end,
+                    Container(
+                      // add gradient in front of building photo to make text in
+                      // front of it legible, based on the example provided here
+                      // https://api.flutter.dev/flutter/widgets/Stack-class.html
+                      alignment: Alignment.bottomRight,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: <Color>[
+                            Colors.black.withAlpha(0),
+                            Colors.black12,
+                            Colors.black38,
+                            Colors.black
+                          ],
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 8),
+                        child: Text(
+                          "Κρατική Πινακοθήκη\nΣύγχρονης Κυπριακής Τέχνης",
+                          style: TextStyle(fontSize: 30),
+                          textAlign: TextAlign.end,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            HeadlineAndMoreRow(listType: "Painting"),
-            PaintingListHorizontal(listType: "Painting"),
-            HeadlineAndMoreRow(listType: "Painter"),
-            PaintingListHorizontal(listType: "Painter"),
-          ],
+              HeadlineAndMoreRow(listType: "Painting"),
+              PaintingListHorizontal(listType: "Painting"),
+              HeadlineAndMoreRow(listType: "Painter"),
+              PaintingListHorizontal(listType: "Painter"),
+            ],
+          ),
         ),
       ),
     );
