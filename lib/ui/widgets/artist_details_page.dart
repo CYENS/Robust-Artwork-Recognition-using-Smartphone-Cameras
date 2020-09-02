0873b0f3fce6517483218e28c018c7ae46728577
marios <msthoma@yahoo.com>
2020-09-02 13:48:51 +0300

    overhaul ArtistDetailsPage, display all available information about artist

--- lib/ui/widgets/artist_details_page.dart
+++ lib/ui/widgets/artist_details_page.dart
@@ -1,4 +1,7 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
+import 'package:flutter_lorem/flutter_lorem.dart';
 import 'package:modern_art_app/data/artworks_dao.dart';
 import 'package:modern_art_app/data/database.dart';
 import 'package:modern_art_app/ui/widgets/item_list.dart';
@@ -11,32 +14,53 @@ class ArtistDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text("Artist details"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Hero(
-            tag: artist.name,
-            child: Container(
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage(artist.fileName), fit: BoxFit.cover),
-                  shape: BoxShape.circle),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Hero(
+              tag: artist.name,
+              child: Container(
+                height: size.height * 0.4,
+                width: size.width,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(artist.fileName), fit: BoxFit.cover)),
+              ),
             ),
-          ),
-          Text(artist.name, style: TextStyle(fontSize: 30)),
-          Text("Description", style: TextStyle(fontSize: 25)),
-          Text(artist.biography),
-          Text("Artworks by ${artist.name}", style: TextStyle(fontSize: 25)),
-          ListHorizontal(
-              itemList: Provider.of<ArtworksDao>(context)
-                  .watchArtworksByArtist(artist)),
-        ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(4.0, 12.0, 4.0, 0.0),
+              child: Text(artist.name, style: TextStyle(fontSize: 30)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text("${artist.yearBirth}–${artist.yearDeath}",
+                  style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic)),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(4.0, 12.0, 4.0, 0.0),
+              child: Text("Short Biography", style: TextStyle(fontSize: 20)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(lorem(
+                  paragraphs: 1, words: 150 - math.Random().nextInt(100))),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(4.0, 12.0, 4.0, 4.0),
+              child: Text("Artworks by ${artist.name}",
+                  style: TextStyle(fontSize: 20)),
+            ),
+            ListHorizontal(
+                itemList: Provider.of<ArtworksDao>(context)
+                    .watchArtworksByArtist(artist)),
+          ],
+        ),
       ),
     );
   }
