b4b7372c87c4b174368f8748566f504618954d47
marios <msthoma@yahoo.com>
2020-09-04 12:55:57 +0300

    add settings page

---
+++ lib/ui/widgets/settings_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SettingsScreen(
+      title: "Settings",
+      children: [],
+    );
+  }
+}
