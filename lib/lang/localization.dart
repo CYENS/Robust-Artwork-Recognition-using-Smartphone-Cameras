514a3dc0e1f0b6803dfc516ebc2d160a5a72006f
marios <msthoma@yahoo.com>
2020-09-10 16:57:40 +0300

    add documentation

--- lib/lang/localization.dart
+++ lib/lang/localization.dart
@@ -6,7 +6,12 @@ import 'package:modern_art_app/data/url_data_sources.dart';
 
 part 'localization.g.dart';
 
+/// Generates a localizations delegate from an online Google Sheet file, see
+/// [flutter\_sheet\_localization](https://github.com/aloisdeniel/flutter_sheet_localization)
+/// library for more information.
 @SheetLocalization(docId, i18nSheetID, 2)
+// The number is the generated version, and must be incremented each time the
+// GSheet is updated, to regenerate a new version of the labels.
 class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
   const AppLocalizationsDelegate();
 
