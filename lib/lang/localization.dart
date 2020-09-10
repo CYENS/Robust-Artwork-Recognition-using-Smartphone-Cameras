b1d5102e5075aea3eee64107bb4f6fa1a45d0b5d
marios <msthoma@yahoo.com>
2020-09-10 14:21:53 +0300

    new AppLocalizationsDelegate class

---
+++ lib/lang/localization.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_sheet_localization/flutter_sheet_localization.dart';
+import 'package:modern_art_app/data/sensitive.dart';
+import 'package:modern_art_app/data/url_data_sources.dart';
+
+part 'localization.g.dart';
+
+@SheetLocalization(docId, i18nSheetID, 1)
+class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+  const AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) =>
+      AppLocalizations.languages.containsKey(locale);
+
+  @override
+  Future<AppLocalizations> load(Locale locale) =>
+      SynchronousFuture<AppLocalizations>(AppLocalizations(locale));
+
+  @override
+  bool shouldReload(AppLocalizationsDelegate old) => false;
+}
