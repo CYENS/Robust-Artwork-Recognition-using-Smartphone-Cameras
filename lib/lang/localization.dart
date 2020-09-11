2deca580ea82a04460b087e03f3910cc8950205f
marios <msthoma@yahoo.com>
2020-09-11 11:27:59 +0300

    add documentation for the terminal commands necessary to auto-generate code

--- lib/lang/localization.dart
+++ lib/lang/localization.dart
@@ -9,6 +9,17 @@ part 'localization.g.dart';
 /// Generates a localizations delegate from an online Google Sheet file, see
 /// [flutter\_sheet\_localization](https://github.com/aloisdeniel/flutter_sheet_localization)
 /// library for more information.
+///
+/// Run the following in the terminal to generate the necessary code:
+///
+/// `flutter packages pub run build_runner build`
+///
+/// or the following to continuously regenerate code when the code changes:
+///
+/// `flutter packages pub run build_runner watch`
+///
+/// In case the commands above report conflicts, append
+/// `--delete-conflicting-outputs` and re-run.
 @SheetLocalization(docId, i18nSheetID, 2)
 // The number is the generated version, and must be incremented each time the
 // GSheet is updated, to regenerate a new version of the labels.
