04db4d42ef993db64a021cc12ffc14ee3135660b
marios <msthoma@yahoo.com>
2020-09-11 12:09:16 +0300

    add more documentation

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -11,13 +11,20 @@ extension Localization on BuildContext {
 extension CustomStringMethods on String {
   /// Extension method on [String] that properly capitalizes Greek strings,
   /// removing any unnecessary accents in the capitalized string. The method
-  /// otherwise works identically to `.toUpperCase()` in other languages. For
-  /// example:
+  /// otherwise works identically to ["string".toUpperCase()] in other
+  /// languages. For example:
   ///
   /// `"Εργα τέχνης"` becomes `"ΕΡΓΑ ΤΕΧΝΗΣ"` and not `"ΈΡΓΑ ΤΈΧΝΗΣ"`
   ///
   /// (NOTE: the accented capital vowels in the example above may not show up
   /// properly here in the IDE, but they usually do in other text editors).
+  ///
+  /// One case that this method does not deal with is vowels with διαλυτικά,
+  /// e.g. ΐ, ΰ, etc.
+  ///
+  /// The [ReCase](https://pub.dev/packages/recase) package may be used to
+  /// achieve this functionality (one disadvantage is that it replaces spaces
+  /// with "_").
   String customToUpperCase() {
     RegExp greek = RegExp(r'[α-ωΑ-Ω]');
     if (this.contains(greek)) {
