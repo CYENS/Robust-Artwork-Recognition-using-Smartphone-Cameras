9cf28fe415fbdc002e9f40e0229b32a6d58185f8
marios <msthoma@yahoo.com>
2020-09-14 12:24:51 +0300

    add documentation

--- asset_import_scripts/CNN/video_processing.py
+++ asset_import_scripts/CNN/video_processing.py
@@ -5,18 +5,29 @@ import skvideo.io
 video_files_dir = "/home/marios/Downloads/contemporary_art_video_files"
 
 
-def get_video_files():
-    return Path(video_files_dir).glob("*.mp4")
+def get_video_files(video_dir: str):
+    """  Gets paths of video files at provided directory.
+
+    :param video_dir: path of dir with video files
+    :return: generator of Paths
+    """
+    return Path(video_dir).glob("*.mp4")
 
 
-def get_video_rotation(file_path: str):
-    metadata = skvideo.io.ffprobe(file_path)
+def get_video_rotation(video_path: str):
+    """ Reads video rotation from video metadata.
+
+    :param video_path: path to the video file
+    :return: rotation of video in int degrees (e.g. 90, 180)
+    :raises AssertionError if rotation is not available, files doesn't exist, or file doesn't have metadata
+    """
+    metadata = skvideo.io.ffprobe(video_path)
     for tags in metadata["video"]["tag"]:
         # we get OrderedDicts here
         if tags["@key"] == "rotate":
-            return tags['@value']
+            return int(tags['@value'])
     else:
-        raise AssertionError(f"Couldn't get rotation for {file_path}, either file doesn't exist, does not contain "
+        raise AssertionError(f"Couldn't get rotation for {video_path}, either file doesn't exist, does not contain "
                              f"metadata, or rotation is not included in its metadata.")
 
 
