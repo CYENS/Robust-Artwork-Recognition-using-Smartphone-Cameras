ccec420b8dd28b86b613cdffd93a925f69657ba0
marios <msthoma@yahoo.com>
2020-09-26 13:10:47 +0300

    use new artwork/artist sheet numbers

--- asset_import_scripts/main.py
+++ asset_import_scripts/main.py
@@ -15,7 +15,7 @@ def get_json(sheet_number):
 
 
 def main():
-    json_types, sheet_numbers = ["artworks", "artists"], [3, 4]
+    json_types, sheet_numbers = ["artworks", "artists"], [8, 9]
 
     for json_type, sheet_number in zip(json_types, sheet_numbers):
         gsheet_json = get_json(sheet_number)
