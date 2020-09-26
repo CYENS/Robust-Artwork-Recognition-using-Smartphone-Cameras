5a3361d9f99ba596d1a6a9ac61d12b6fe88f6596
marios <msthoma@yahoo.com>
2020-09-26 13:11:25 +0300

    slight modification to regex pattern to handle an edge case

--- asset_import_scripts/main.py
+++ asset_import_scripts/main.py
@@ -23,7 +23,7 @@ def main():
         print(gsheet_json.text)
 
         # remove links to spreadsheet and personal information (email)
-        pattern = re.compile(r'https://.+?\.google\.com/.+?\"|\"m.+?ma@*')
+        pattern = re.compile(r'https://.+?\.google\.com/.+?\"|\"ms.+?ma@*')
 
         for i in re.findall(pattern, gsheet_json.text):
             print(i)
