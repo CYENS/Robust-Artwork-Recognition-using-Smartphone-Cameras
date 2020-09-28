4f1b1c2b993b90e66cf737e783b7ea1f2498e89e
marios <msthoma@yahoo.com>
2020-09-28 17:54:55 +0300

    new db query that takes locale as an argument, and returns a list of Artworks translated to that specific locale

--- lib/data/artworks_dao.dart
+++ lib/data/artworks_dao.dart
@@ -4,13 +4,35 @@ import 'package:moor/moor.dart';
 part 'artworks_dao.g.dart';
 
 /// Data access object for [Artwork] database related operations.
-@UseDao(tables: [Artworks])
+@UseDao(tables: [Artworks, ArtworkTranslations, ArtistTranslations])
 class ArtworksDao extends DatabaseAccessor<AppDatabase>
     with _$ArtworksDaoMixin {
   ArtworksDao(AppDatabase db) : super(db);
 
   /// Gets a list of all artworks in the db.
   Future<List<Artwork>> get allArtworkEntries => select(artworks).get();
+
+  Stream<List<Artwork>> allArtworkEntries2({String languageCode = "en"}) =>
+      (select(artworks).join([
+        leftOuterJoin(
+            artworkTranslations, artworkTranslations.id.equalsExp(artworks.id)),
+        leftOuterJoin(artistTranslations,
+            artistTranslations.id.equalsExp(artworks.artistId))
+      ])
+            ..where(artworkTranslations.languageCode.equals(languageCode) &
+                artistTranslations.languageCode.equals(languageCode)))
+          .watch()
+          .map((entries) => entries.map((entry) {
+                Artwork artwork = entry.readTable(artworks);
+                ArtworkTranslation artworkTranslation =
+                    entry.readTable(artworkTranslations);
+                artwork = artwork.copyWith(
+                    name: artworkTranslation.name,
+                    description: artworkTranslation.description,
+                    artist: entry.readTable(artistTranslations).name);
+                print(artwork);
+                return artwork;
+              }).toList());
 
   /// Gets a live stream of all artworks in the db (automatically emits new items
   /// whenever the underlying data changes).
