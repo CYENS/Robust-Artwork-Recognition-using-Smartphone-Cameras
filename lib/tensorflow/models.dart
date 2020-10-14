4952c44cedb8b670cc9a4c51f9f3a558ebd27eb5
marios <msthoma@yahoo.com>
2020-10-14 15:41:03 +0300

    new simple class to hold information about bundled tflite models

--- lib/tensorflow/models.dart
+++ lib/tensorflow/models.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 const String mobilenet = "MobileNet";
 const String ssd = "SSD MobileNet";
 const String yolo = "Tiny YOLOv2";
@@ -11,3 +13,19 @@ const String vgg19ZeroOneMultiQuant = "VGG19 - 0, 1, or Multiple (Quantized)";
 const String mobileNetNoArt = "MobileNet - No Artwork category";
 const String mobileNetNoArtQuant =
     "MobileNet - No Artwork category (Quantized)";
+
+class TfLiteModel {
+  final String modelAssetsPath = "assets/tflite/";
+
+  final String _modelPath;
+
+  final String _labelsPath;
+
+  const TfLiteModel({@required String modelPath, @required String labelsPath})
+      : _modelPath = modelPath,
+        _labelsPath = labelsPath;
+
+  String get modelPath => modelAssetsPath + _modelPath;
+
+  String get labelsPath => modelAssetsPath + _labelsPath;
+}
