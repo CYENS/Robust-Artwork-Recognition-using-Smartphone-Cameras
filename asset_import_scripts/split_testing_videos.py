72a4ee6abece1803f14fc23de884c66c73e2618e
marios <msthoma@yahoo.com>
2020-11-14 17:23:17 +0200

    add notifications for success and errors

--- asset_import_scripts/split_testing_videos.py
+++ asset_import_scripts/split_testing_videos.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import xml.etree.ElementTree as ET
 from optparse import OptionParser
 from pathlib import Path
@@ -16,13 +17,17 @@ def save_vlc_timestamp(clip_type: str):
      Downward, Upward, Left, and Right with the corresponding letters [f, d, u, l, r]
     """
     if clip_type not in "fdulr":
-        print("Clip type provided is not acceptable, should be one of [f, d, u, l, r]")
+        notify("Clip type provided is not acceptable, should be one of [f, d, u, l, r]")
         raise SystemExit
 
     # TODO have dict with file-artwork name associations, so can append the artwork here
 
     # request to VLC
     r = requests.get("http://127.0.0.1:8080/requests/status.xml", auth=("", "abc"))
+    if r.status_code != 200:
+        notify(f"Problem requesting information from VLC, status code: {r.status_code}")
+        raise SystemExit
+
     # return status information is in xml form
     root = ET.fromstring(r.text)
 
@@ -32,7 +37,7 @@ def save_vlc_timestamp(clip_type: str):
             video_name = i[0].text
             break
     if video_name == "":
-        print("Couldn't get current video filename!")
+        notify("Couldn't get current video filename!")
         raise SystemExit
 
     percent = root.find("position").text  # current position as a percentage
@@ -51,6 +56,12 @@ def save_vlc_timestamp(clip_type: str):
 
     with open(csv_path, "a") as f:
         f.write(",".join(info) + "\n")
+
+    notify(f"Saved {round(float(timestamp), 2)} timestamp for {clip_type}-{video_name}")
+
+
+def notify(msg: str):
+    subprocess.Popen(['notify-send', msg])
 
 
 def main():
