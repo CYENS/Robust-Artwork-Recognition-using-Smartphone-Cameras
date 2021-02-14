7fa9bc9d6feb5a687bd8bc7d6d4a30a4bb65e9ff
marios <msthoma@yahoo.com>
2021-02-14 18:14:36 +0200

    remove cupertino import

--- lib/ui/pages/explore_page.dart
+++ lib/ui/pages/explore_page.dart
@@ -1,5 +1,4 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:modern_art_app/data/artists_dao.dart';
