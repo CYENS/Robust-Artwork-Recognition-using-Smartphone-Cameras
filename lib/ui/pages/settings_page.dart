f748c1d7d2dc5fd5b665e5df470b22a4c5f37563
marios <msthoma@yahoo.com>
2021-02-15 14:29:06 +0200

    fix default value of _navigateToDetails being false

--- lib/ui/pages/settings_page.dart
+++ lib/ui/pages/settings_page.dart
@@ -189,9 +189,9 @@ class _SettingsPageState extends State<SettingsPage> {
                     ),
                   ),
                 ],
-              )
+              ),
             ],
-          )
+          ),
         ],
       ),
     );
