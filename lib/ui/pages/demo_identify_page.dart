0af81313b6b584f00f582b6de665fe2f9d1a418a
marios <msthoma@yahoo.com>
2021-12-21 11:13:43 +0200

    remove unneeded import

--- lib/ui/pages/demo_identify_page.dart
+++ lib/ui/pages/demo_identify_page.dart
@@ -1,6 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:modern_art_app/utils/extensions.dart';
 
@@ -20,10 +19,7 @@ class DemoIdentifyPage extends StatelessWidget {
         child: Stack(
           fit: StackFit.expand,
           children: [
-            Image.asset(
-              "assets/example_frame.webp",
-              fit: BoxFit.fitHeight,
-            ),
+            Image.asset("assets/example_frame.webp", fit: BoxFit.fitHeight),
             Positioned.fill(
               child: Padding(
                 padding: const EdgeInsets.fromLTRB(4, 4, 4, 100),
